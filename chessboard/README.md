e630d9a1890e284db05c65b04a4edfd599ff51b8
asisow <38430905+asisow@users.noreply.github.com>
2018-09-28 15:59:53 +0300

    Select proper side/ change side radio

---
+++ chessboard/README.md
@@ -0,0 +1,1 @@
+# asisow.github.io
--- chessboard/chessScript.js
+++ chessboard/chessScript.js
@@ -1,25 +1,14 @@
-let letterRow = 'abcdefgh';
+
 
 let removePiece = function (object) {
     let piece = object.firstChild;
-    object.removeChild(object.firstChild);
+    object.removeChild.firstChild;
 };
 
 let clearMCross = () => {
-    document.querySelector('#initial').value = '';
-    document.querySelector('#target').value = '';
-    document.querySelector('#typeOfMove').value = '';
+    
 }
 
-let lightenMoveArea = (piece) => {
-    if (piece.classList[0] === 'pawn') {
-        let x = piece.parent.className[0];
-        let y = piece.parent.className[1];
-        for (let i = 1; i < 2; i += 1) {
-
-        }
-    }
-}
 
 let makeAmove = function () {
     let side = document.querySelector('input[type=radio]:not(:checked)');
@@ -31,14 +20,13 @@ let makeAmove = function () {
     initial.removeChild(piece);
     final.appendChild(piece);
     side.checked = true;
-    clearMCross();
 };
 
 let clearBoard = function () {
     let list = document.querySelectorAll('.row li');
     for (let i = 0; i < list.length; i += 1) {
         let item = list[i].querySelector('div');
-        item.removeChild();
+        item.removeAttribute('class');
     }
 };
 
@@ -48,15 +36,16 @@ let selectPiece = function (elem) {
     let side = document.querySelector('input[type=radio]:checked').value;
     if (piece === side) {
         document.querySelector('#initial').value = elem.className;
-        lightenMoveArea(elem.firstChild);
     }
-    else if (piece != side) {
-        document.querySelector('#typeOfMove').value = 'X';
+    else {
         document.querySelector('#target').value = elem.className;
     }
-   }
-  else {
-      document.querySelector('#typeOfMove').value = ':';
-      document.querySelector('#target').value = elem.className;
   }
+  else {
+    document.querySelector('#target').value = elem.className;
+  }
+};
+
+let selectBlankPiece = function (elem) {
+    document.querySelector('#target').value = elem.className;
 };
