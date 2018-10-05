c4d6d4d8d31268a1c915502d1027d72109c2229c
asisow <38430905+asisow@users.noreply.github.com>
2018-10-05 16:26:46 +0300

    Ooow! transfered drawTable to js

--- chessboard/chessScript.js
+++ chessboard/chessScript.js
@@ -1,55 +1,9 @@
-let letterRow = 'abcdefgh';
-
-let removePiece = function (object) {
-    let piece = object.firstChild;
-    object.removeChild(object.firstChild);
-};
-
-let clearMCross = () => {
-    document.querySelector('#initial').value = '';
-    document.querySelector('#target').value = '';
-    document.querySelector('#typeOfMove').value = '';
-}
-
-let makeAnOption = (piece, initialPlacement, targetPlacement) => {
-    let select = document.querySelector('#hist');
-    let type = document.querySelector('#typeOfMove').value;
-    let option = document.createElement('option')
-    option.innerHTML =  piece.classList[0] + initialPlacement + type + targetPlacement;
-    select.appendChild(option);
-}
-// Make a move function
-let makeAmove = function () {
-    //Check if movementCross has data
-    if (document.querySelector('#initial').value && document.querySelector('#target').value) {
-        //Get the player side to check
-        let side = document.querySelector('input[type=radio]:not(:checked)');
-        //Get the initial and target coordinates
-    let initialPlacement = document.querySelector('#initial').value;
-    let targetPlacement = document.querySelector('#target').value;
-        let final = document.querySelector('.' + targetPlacement);
-        let initial = document.querySelector('.' + initialPlacement);
-        let piece = initial.firstChild;
-        initial.removeChild(piece);
-        final.appendChild(piece);
-        side.checked = true;
-        clearMCross();
-        makeAnOption(piece, initialPlacement, targetPlacement);
-    }
-};
-
-let clearBoard = function () {
-    let list = document.querySelectorAll('.row li');
-    for (let i = 0; i < list.length; i += 1) {
-        if (list[i].hasChildNodes()) {
-            let item = list[i].querySelector('div');
-            list[i].removeChild(list[i].firstChild);
-        }
-    }
-};
+const letterRow = 'abcdefgh';
+const figureView = "♕♔♗♘♖♙♛♚♝♞♜♟";
+//
 
 let selectPiece = function (elem) {
-  if (elem.hasChildNodes()) {
+  if (elem.hasChildNodes) {
     let piece = elem.firstChild.classList[1];
     let side = document.querySelector('input[type=radio]:checked').value;
     lightenMoveArea(elem.firstChild);
@@ -67,6 +21,71 @@ let selectPiece = function (elem) {
       document.querySelector('#target').value = elem.className;
   }
 
+};
+let drawBoard = () => {
+    for (let i = 0; i < 8; i += 1) {
+        let board = document.querySelector('.board');
+        let row = document.createElement('ul');
+        let numberMark = String(8 - i);
+        for (j = 0; j < 8; j += 1) {
+            let cell = document.createElement('li');
+            let letterMark = letterRow[j];
+            cell.classList.add(letterMark + numberMark);
+            cell.innerHTML = letterMark + numberMark;
+            cell.addEventListener('click', selectPiece(self), false);
+            row.appendChild(cell);
+        }
+        row.classList.add('row');
+        board.appendChild(row);
+    }
+}
+
+
+
+let removePiece = function (object) {
+    let piece = object.firstChild;
+    object.removeChild(object.firstChild);
+};
+
+let clearMCross = () => {
+    document.querySelector('#initial').value = '';
+    document.querySelector('#target').value = '';
+    document.querySelector('#typeOfMove').value = '';
+}
+
+let makeAnOption = (piece) => {
+    let select = document.querySelector('#hist');
+    let type = document.querySelector('#typeOfMove').value;
+    let option = document.createElement('option')
+    option.innerHTML =  piece.classList[0] + document.querySelector('#initial').value + type + document.querySelector('#target').value;
+    select.appendChild(option);
+}
+// Make a move function
+let makeAmove = function () {
+    //Check if movementCross has data
+    if (document.querySelector('#initial').value && document.querySelector('#target').value) {
+        //Get the player side to check
+        let side = document.querySelector('input[type=radio]:not(:checked)');
+        //Get the initial and target coordinates
+        let final = document.querySelector('.' + document.querySelector('#target').value);
+        let initial = document.querySelector('.' + document.querySelector('#initial').value);
+        let piece = initial.firstChild;
+        initial.removeChild(piece);
+        final.appendChild(piece);
+        side.checked = true;
+        makeAnOption(piece);
+        clearMCross();
+    }
+};
+
+let clearBoard = function () {
+    let list = document.querySelectorAll('.row li');
+    for (let i = 0; i < list.length; i += 1) {
+        if (list[i].hasChildNodes()) {
+            let item = list[i].querySelector('div');
+            list[i].removeChild(list[i].firstChild);
+        }
+    }
 };
 
 let lightenMoveArea = (piece) => {
