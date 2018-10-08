6db1f731d4ad103ce6643dbfe97ab93a1aaeb17d
asisow <38430905+asisow@users.noreply.github.com>
2018-10-08 20:50:04 +0300

    Update Calendar/calendar.js

--- Calendar/calendar.js
+++ Calendar/calendar.js
@@ -13,11 +13,13 @@ let daysInMonths = {'January': 31,
 'February': 31};
 let months = ['January','February','March','April','May','June','July','August','September','October','November','February'];
 let currentDate = new Date();
+<<<<<<< HEAD
 for (i = 1; i <= months[currentDate.getMonth()]; i += 1) {
         let currentMonth = documentQuerySelector('.first-month');
         let daysinMonth = daysInMonths[currentMonth];
         console.log(daysInMonth)
     }
+=======
     console.log(months[currentDate.getMonth()]);
 for (i = 1; i <= months[currentDate.getMonth()]; i += 1) {
         let currentMonth = documentQuerySelector('.first-month');
