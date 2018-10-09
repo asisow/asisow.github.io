db8e2e433e6a11927074902a2996801816609e5a
asisow <38430905+asisow@users.noreply.github.com>
2018-10-09 16:40:58 +0300

    made make a calendar functions

--- Calendar/calendar.js
+++ Calendar/calendar.js
@@ -1,4 +1,20 @@
-let daysInYear = 365
+let createElement = (type, cls)=> {
+    let elem = document.createElement(type);
+    console.log(elem)
+    elem.className = cls;
+    return elem
+}
+
+let createDayBlock = (dateNumber, dayOfWeek, type) => {
+    let cls = 'date-' + type + ' ' + dayOfWeek;
+    let block = createElement('a', cls);
+    console.log(week[dayOfWeek]);
+    type = 'date-' + type;
+    block.href = '#';
+    return block;
+}
+
+let daysInYear = 365;
 let daysInMonths = {'January': 31,
 'February': 28,
 'March': 31,
@@ -10,10 +26,19 @@ let daysInMonths = {'January': 31,
 'September': 30,
 'October': 31,
 'November': 30,
-'February': 31};
-let months = ['January','February','March','April','May','June','July','August','September','October','November','February'];
+'December': 31};
+const board = document.querySelector(".calendar");
+let months = Object.keys(daysInMonths) //['January','February','March','April','May','June','July','August','September','October','November','February'];
 let currentDate = new Date();
-    console.log(months[currentDate.getMonth()]);
-for (i = 1; i <= months[currentDate.getMonth()]; i += 1) {
-        let currentMonth = documentQuerySelector('.first-month');
-    }
+let currentMonth = currentDate.getMonth();
+let week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', ]
+
+let firstDayOfMonth = new Date(currentDate.getFullYear(), currentMonth);
+let weekDayOfMonth = firstDayOfMonth.getDay();
+for (i = 1; i <= daysInMonths[months[currentDate.getMonth()]]; i += 1) {
+    let dayOfWeek = week[weekDayOfMonth];
+    let block = createDayBlock(i, dayOfWeek, 'thisMonth')
+    block.innerHTML = i;
+    board.appendChild(block);
+    weekDayOfMonth = (weekDayOfMonth + 1) % 7;
+}
