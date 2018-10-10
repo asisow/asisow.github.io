a134b2206572bc1a352268eb18d3dbe36193cdb3
Andrey <asisow@gmail.com>
2018-10-10 17:02:05 +0300

    Leave what is good

--- Calendar/calendar.js
+++ Calendar/calendar.js
@@ -1,10 +1,10 @@
-
+//createElement function
 let createElement = (type, cls)=> {
     let elem = document.createElement(type);
     elem.className = cls;
     return elem
 }
-
+//createDayBlock uses createElement
 let createDayBlock = (dateNumber, dayOfWeek, type) => {
     let cls = 'date-' + type + ' ' + dayOfWeek;
     let block = createElement('a', cls);
@@ -12,29 +12,31 @@ let createDayBlock = (dateNumber, dayOfWeek, type) => {
     block.href = '#';
     return block;
 }
-const daysInYear = 365;
+//global variables **need to add if statement to determine leap year**
+let daysInYear = 365;
 let daysInMonths = {'January': 31,
-'February': 28,
-'March': 31,
-'April': 30,
-'May': 31,
-'June': 30,
-'July': 31,
-'August': 31,
-'September': 30,
-'October': 31,
-'November': 30,
-'December': 31};
+                    'February': 28,
+                    'March': 31,
+                    'April': 30,
+                    'May': 31,
+                    'June': 30,
+                    'July': 31,
+                    'August': 31,
+                    'September': 30,
+                    'October': 31,
+                    'November': 30,
+                    'December': 31};
 const board = document.querySelector(".calendar");
 const months = Object.keys(daysInMonths) //['January','February','March','April','May','June','July','August','September','October','November','February'];
 let currentDate = new Date();
 let currentMonth = currentDate.getMonth();
 let week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', ]
+//determine previous month days quantity and day of week of previous month
 let previousMonthDaysQuantity = daysInMonths[months[currentDate.getMonth() - 1]]
 let dayPreviousMonthsWeek = previousMonthDaysQuantity - currentDate.getDay();
-
 let firstDayOfMonth = new Date(currentDate.getFullYear(), currentMonth);
 let weekDayOfMonth = firstDayOfMonth.getDay();
+//fill previous month days if first of current month is no a monday
 if (firstDayOfMonth.getDay() > 1) {
     for (let i =  previousMonthDaysQuantity - firstDayOfMonth.getDay() + 2; i <= previousMonthDaysQuantity; i += 1) {
         let dayOfWeek = 0;
@@ -44,6 +46,7 @@ if (firstDayOfMonth.getDay() > 1) {
         dayOfWeek += 1;
     }
 }
+//fill current month
 for (i = 1; i <= daysInMonths[months[currentDate.getMonth()]]; i += 1) {
     let dayOfWeek = week[weekDayOfMonth];
     let block = createDayBlock(i, dayOfWeek, 'thisMonth')
@@ -51,7 +54,9 @@ for (i = 1; i <= daysInMonths[months[currentDate.getMonth()]]; i += 1) {
     board.appendChild(block);
     weekDayOfMonth = (weekDayOfMonth + 1) % 7;
 }
+//determine left days to fill
 let leftDays = 8 - weekDayOfMonth;
+//fill left tiles
 if (weekDayOfMonth < 7) {
     for (let i = 1; i <=  leftDays; i += 1) {
         let block1 = createDayBlock(i, week[weekDayOfMonth], 'grey')
