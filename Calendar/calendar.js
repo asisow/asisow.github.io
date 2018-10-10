f57fafcf14ee85b1477d395829b8882192c5556a
asisow <38430905+asisow@users.noreply.github.com>
2018-10-10 16:01:32 +0300

    added box-shadows
    to make tiles  very solid yummy

--- Calendar/calendar.js
+++ Calendar/calendar.js
@@ -1,6 +1,5 @@
 let createElement = (type, cls)=> {
     let elem = document.createElement(type);
-    console.log(elem)
     elem.className = cls;
     return elem
 }
@@ -8,13 +7,11 @@ let createElement = (type, cls)=> {
 let createDayBlock = (dateNumber, dayOfWeek, type) => {
     let cls = 'date-' + type + ' ' + dayOfWeek;
     let block = createElement('a', cls);
-    console.log(week[dayOfWeek]);
     type = 'date-' + type;
     block.href = '#';
     return block;
 }
-
-let daysInYear = 365;
+const daysInYear = 365;
 let daysInMonths = {'January': 31,
 'February': 28,
 'March': 31,
@@ -28,13 +25,24 @@ let daysInMonths = {'January': 31,
 'November': 30,
 'December': 31};
 const board = document.querySelector(".calendar");
-let months = Object.keys(daysInMonths) //['January','February','March','April','May','June','July','August','September','October','November','February'];
+const months = Object.keys(daysInMonths) //['January','February','March','April','May','June','July','August','September','October','November','February'];
 let currentDate = new Date();
 let currentMonth = currentDate.getMonth();
 let week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', ]
+let previousMonthDaysQuantity = daysInMonths[months[currentDate.getMonth() - 1]]
+let dayPreviousMonthsWeek = previousMonthDaysQuantity - currentDate.getDay();
 
 let firstDayOfMonth = new Date(currentDate.getFullYear(), currentMonth);
 let weekDayOfMonth = firstDayOfMonth.getDay();
+if (firstDayOfMonth.getDay() > 1) {
+    for (let i =  previousMonthDaysQuantity - firstDayOfMonth.getDay() + 2; i <= previousMonthDaysQuantity; i += 1) {
+        let dayOfWeek = 0;
+        let block1 = createDayBlock(i, week[dayOfWeek], 'grey')
+        block1.innerHTML = i;
+        board.appendChild(block1);
+        dayOfWeek += 1;
+    }
+}
 for (i = 1; i <= daysInMonths[months[currentDate.getMonth()]]; i += 1) {
     let dayOfWeek = week[weekDayOfMonth];
     let block = createDayBlock(i, dayOfWeek, 'thisMonth')
@@ -42,4 +50,13 @@ for (i = 1; i <= daysInMonths[months[currentDate.getMonth()]]; i += 1) {
     board.appendChild(block);
     weekDayOfMonth = (weekDayOfMonth + 1) % 7;
 }
-
+let leftDays = 8 - weekDayOfMonth;
+if (weekDayOfMonth < 7) {
+    for (let i = 1; i <=  leftDays; i += 1) {
+        let block1 = createDayBlock(i, week[weekDayOfMonth], 'grey')
+        block1.innerHTML = i;
+        board.appendChild(block1);
+        weekDayOfMonth = (weekDayOfMonth + 1) % 7;
+        console.log(weekDayOfMonth);
+    }
+}
